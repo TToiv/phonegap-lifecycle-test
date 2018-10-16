c19d14b09bc9f0f5f99f44fc8816bfc3c48f6497
TToiv <toit1_15@uni.worc.ac.uk>
2018-10-16 14:38:44 +0100

    added key

--- www/js/index.js
+++ www/js/index.js
@@ -42,3 +42,16 @@ function updateDisplay() {
 	    
 	alert("resume");
     }
+
+
+var key = "pen";
+var value = "blue";
+
+window.localStorage.setItem(key, value);
+
+alert(key + " is " + value);
+
+var key = "pen";
+var value = window.localStorage.getItem(key);
+
+alert(key + " is " + value);
