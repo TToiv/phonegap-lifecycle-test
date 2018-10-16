75260748fd9e5971189bde7db7900036ffc10c42
TToiv <toit1_15@uni.worc.ac.uk>
2018-10-16 15:00:37 +0100

    added items

--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,44 @@
 var key = "pen";
 var value = "blue";
-
 window.localStorage.setItem(key, value);
 
 var key = "phone";
 var value = "black";
-
 window.localStorage.setItem(key, value);
 
+var key = "keys";
+var value = "silver";
+window.localStorage.setItem(key, value);
+
+var key = "wallet";
+var value = "black";
+window.localStorage.setItem(key, value);
+
+var key = "receipt";
+var value = "white";
+window.localStorage.setItem(key, value);
+
+var key = "pen";
+var value = window.localStorage.getItem(key);
 
 alert(key + " is " + value);
 
-var key = "pen";
+var key = "phone";
+var value = window.localStorage.getItem(key);
+
+alert(key + " is " + value);
+
+var key = "keys";
+var value = window.localStorage.getItem(key);
+
+alert(key + " is " + value);
+
+var key = "wallet";
+var value = window.localStorage.getItem(key);
+
+alert(key + " is " + value);
+
+var key = "receipt";
 var value = window.localStorage.getItem(key);
 
 alert(key + " is " + value);
